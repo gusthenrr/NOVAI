10a69183809483f334074232f7310f4f219945a4
gusthenrr <154958506+gusthenrr@users.noreply.github.com>
2025-08-07 15:44:32 -0300

    Add files via upload

--- next.js/next.config.ts
+++ next.js/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint:{
+    ignoreDuringBuilds: true, // Ignora erros de linting durante a construção
+  }
 };
 
 export default nextConfig;
