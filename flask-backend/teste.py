2a7ef540341c7d21ae2d4424cb62a9c1bfab33d7
gusthenrr <154958506+gusthenrr@users.noreply.github.com>
2025-08-15 14:24:31 -0300

    Update teste.py

--- flask-backend/teste.py
+++ flask-backend/teste.py
@@ -3247,10 +3247,11 @@ def chat_novai_manager_pilot(pergunta : str, user_id : int):
 
 @app.route('/chat_novai_manager', methods=['POST'])
 async def chat_novai_manager_requisicao():
+    token = request.cookies.get('token')
     data = request.get_json()
     print(data)
     mensagem = data.get('message')
-    token = request.cookies.get('token')
+    
     
     if not token:
         print('sem token')
@@ -3890,6 +3891,7 @@ def chat_novai_manager_table_verification(tables : list,mensagem: str,user_id: i
 # 🚀 Rodar o servidor
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000, debug=False)
+
 
 
 
