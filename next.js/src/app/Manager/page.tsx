52bc283eae6e907ff01dff0e3a20056650104423
gusthenrr <154958506+gusthenrr@users.noreply.github.com>
2025-09-04 16:52:02 -0300

    Update page.tsx

--- next.js/src/app/Manager/page.tsx
+++ next.js/src/app/Manager/page.tsx
@@ -748,7 +748,7 @@ const conversationList: ConversationListItem[] = useMemo(() => {
             <img
             src="/Novai_sem_fundo_branco.png" // Caminho da imagem na pasta "public"
             alt="Logo Novai"
-            style={{ width: "70%", height: "auto", justifyContent:"center", alignItems:"center"}}
+            style={{ width: "50%", height: "auto", justifyContent:"center", alignItems:"center"}}
           />
           )}
         </div>
@@ -1204,6 +1204,7 @@ inputField: {
     lineHeight: '1.5',
   }
 };
+
 
 
 
