c3c74f7998fdf6b1720343e2c0a47dbe297dcaad
gusthenrr <154958506+gusthenrr@users.noreply.github.com>
2025-09-04 14:33:26 -0300

    Update teste.py

--- flask-backend/teste.py
+++ flask-backend/teste.py
@@ -3517,7 +3517,7 @@ def chat_novai_manager_requisicao():
         return
     with get_db_connection() as conn, conn.cursor() as cur:
             cur.execute("INSERT INTO history_messages (mensagem, id_conversa, usuario_id_history, data_envio, author) VALUES (%s, %s, %s, %s, %s)",(mensagem, id_conversa, user_id, date, 'user'))    
-    model = ChatOpenAI(model='gpt-5-nano')
+    model = ChatOpenAI(model='gpt-4o-mini')
     descricao_db = '''
 Descrição do banco de dados PostgreSQL:
 
@@ -4157,6 +4157,7 @@ para que uma segunda IA faça os cálculos.
 # 🚀 Rodar o servidor
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000, debug=False)
+
 
 
 
