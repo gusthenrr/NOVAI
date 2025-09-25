4a2b170fe303e304789901cf860e90faa9c19f7e
gusthenrr <154958506+gusthenrr@users.noreply.github.com>
2025-09-25 19:41:04 -0300

    Refactor dynamic import for DashboardClient component

--- next.js/src/app/dashboard/page.tsx
+++ next.js/src/app/dashboard/page.tsx
@@ -1,9 +1,10 @@
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 
-import dynamic from 'next/dynamic';
-const DashboardClient = dynamic(() => import('./DashboardClient'), { ssr: false });
+import NextDynamic from 'next/dynamic';
+const DashboardClient = NextDynamic(() => import('./DashboardClient'), { ssr: false });
 
 export default function Page() {
   return <DashboardClient />;
 }
+
