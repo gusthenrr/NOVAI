e8c5d21f5d782f6e5c49b9f9dd8b1001222b2f9d
gusthenrr <154958506+gusthenrr@users.noreply.github.com>
2025-09-30 20:38:11 -0300

    Update teste.py

--- flask-backend/teste.py
+++ flask-backend/teste.py
@@ -598,6 +598,35 @@ def visitsItems():
     resposta= response.json()
     return resposta
 
+@app.route('/visitas_por_mes', methods=['POST'])
+def visitas_por_mes():
+    with get_db_connection() as conn, conn.cursor() as cur:
+        cur.execute('SELECT acess_token FROM tokens LIMIT 1')
+        acess_token_dict = cur.fetchone()
+        token = acess_token_dict['acess_token']
+    data = request.get_json()
+    conversion = data.get('conversion')
+    item = data.get('item_id')
+    price = data.get('price', 0)
+    print("Buscando visualizações...")
+    total_visits_mes=0
+    date_to= datetime.datetime.now().strftime('%Y-%m-%d')
+    meses = []
+    faturamentos = []
+    for i in range(0,24):
+        date_from= (datetime.datetime.now() - datetime.timedelta(days=(i+1)*30)).strftime('%Y-%m-%d')
+        date_to= (datetime.datetime.now() - datetime.timedelta(days=i*30)).strftime('%Y-%m-%d')
+        url_por_mes= f'https://api.mercadolibre.com/items/visits?ids={item}&date_from={date_from}&date_to={date_to}'
+        response = requests.get(url_por_mes, headers={"Authorization": f"Bearer {token}"})
+        visitas=response.json()
+        date_from= (datetime.datetime.now() - datetime.timedelta(days=(i+1)*30)).strftime('%Y-%m-%d')
+        print(f'Mês: {date_from}', end=' ')
+        total_visits_mes=int(visitas[0]['total_visits'])
+        meses.append(total_visits_mes)
+        faturamentos.append(int(total_visits_mes * (conversion/100))*price)
+        print(f'{i+1}: ',total_visits_mes)
+    print("Status:", response.status_code)
+    return {'meses': meses, 'faturamentos': faturamentos}
 
 def claims_notifications(data, acess_token_data):
     try:
@@ -4709,6 +4738,7 @@ para que uma segunda IA faça os cálculos.
 # 🚀 Rodar o servidor
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000, debug=False)
+
 
 
 
