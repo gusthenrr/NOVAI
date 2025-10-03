24d893bb69dac2476094f8adac0b8d3fbdfe59e7
gusthenrr <154958506+gusthenrr@users.noreply.github.com>
2025-10-03 14:49:40 -0300

    Delete next.js/src/app/dashboard/page.tsx

--- next.js/src/app/dashboard/page.tsx
+++
@@ -1,7 +0,0 @@
-export const dynamic = 'force-dynamic';
-export const revalidate = 0;
-import DashboardClient from './DashboardClient'; // ✅ importe direto
-
-export default function Page() {
-  return <DashboardClient />; // Client Component renderiza no cliente automaticamente
-}
