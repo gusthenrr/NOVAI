5f8d458b061ef8c2bc6330ef4daa585f98c0414f
gusthenrr <154958506+gusthenrr@users.noreply.github.com>
2025-10-03 14:50:25 -0300

    Add files via upload

---
+++ next.js/src/app/dashboard/inicio/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+import DashboardClient from '../DashboardClient'; // ✅ importe direto
+
+export default function Page() {
+  return <DashboardClient />; // Client Component renderiza no cliente automaticamente
+}
